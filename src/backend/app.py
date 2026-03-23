2248a9883cbe94da5b862bd7f24f31771f1077c2
Garry <garrymcbride1993@hotmail.co.uk>
2026-03-23 13:22:02 +0000

    initial creation of endpoint for homepahe of site

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import (
+    HTMLResponse
+)
 from pydantic import BaseModel
 import base64
+from starlette.templating import _TemplateResponse
 
 # Initialize FastAPI application
 app = FastAPI(title = "B&S Auto")
@@ -16,6 +20,30 @@ class FormData(BaseModel):
 def submit_form(form: FormData): # Todo add return type
     
     return {"status": "ok"}
+
+
+# Endpoint for the first page the user will see
+@app.get("/", response_class=HTMLResponse)
+def read_homepage(request: Request) -> _TemplateResponse:
+    """Renders the homepage template.
+
+    Parameters
+    ----------
+    request : Request
+        The incoming HTTP request object.
+
+
+    Returns
+    -------
+    reponse : _TemplateResponse
+        HTML template reponse containing the homepage.
+    
+    """
+    # TODO: Add logging to track homepage access
+    # logging.info("Homepage accessed")
+
+    # TODO: Implement ninja for this - see docaid
+    return templates.TemplateResponse("new_index.html", {"request": request})
 
 
 
