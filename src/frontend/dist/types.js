01d6b394424a20deeb0d42ab32a1536ecf6d21a0
garry.mcbride <garrymcbride1993@hotmail.co.uk>
2026-09-18 21:39:22 +0100

    chore: added javascript files to gitignore

--- src/frontend/dist/types.js
+++
@@ -1,6 +0,0 @@
-/**
- * Adds static typing to JavaScript to catch errors during development.
- * Provides better tooling (autocomplete/refactoring) and enforces
- * data structures to ensure code reliability before it ever runs.
- */
-export {};
